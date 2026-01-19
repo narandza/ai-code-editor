562aea36d8771e46db5deeb33637eaea51d6793d
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2026-01-19 12:28:13 +0100

    build project lists ul

--- src/features/projects/components/projects-list.tsx
+++ src/features/projects/components/projects-list.tsx
@@ -1,5 +1,6 @@
 import { Spinner } from "@/components/ui/spinner";
 import { useProjectsPartial } from "../hooks/use-projects";
+import { Kbd } from "@/components/ui/kbd";
 
 interface ProjectsListProps {
   onViewAll: () => void;
@@ -16,13 +17,25 @@ export const ProjectsList = ({ onViewAll }: ProjectsListProps) => {
 
   return (
     <div className="flex flex-col gap-4">
-      {rest.length > 0 && (
+      {projects.length > 0 && (
         <div className="flex flex-col gap-2">
           <div className="flex items-center justify-between gap-2">
             <span className="text-xs text-muted-foreground">
               Recent projects
             </span>
+            <button className="flex items-center gap-2 text-muted-foreground text-xs hover:text-foreground transition-colors">
+              <span className="">View all</span>
+              <Kbd className="bg-accent border">⌘K</Kbd>
+            </button>
           </div>
+          <ul className="flex flex-col ">
+            {projects.map((project) => (
+              <p className="" key={project._id}>
+                TODO project item
+              </p>
+              // <ProjectItem key={project._id} data={project} />
+            ))}
+          </ul>
         </div>
       )}
     </div>
