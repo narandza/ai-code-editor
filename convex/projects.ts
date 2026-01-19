5c0620b27beeb5f12b80e30382052aa0c5215710
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2026-01-19 11:48:48 +0100

    modify get method to getPartial method

--- convex/projects.ts
+++ convex/projects.ts
@@ -20,10 +20,12 @@ export const create = mutation({
   },
 });
 
-export const get = query({
-  args: {},
-  handler: async (ctx) => {
-    const identity = await ctx.auth.getUserIdentity();
+export const getPartial = query({
+  args: {
+    limit: v.number(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await verifyAuth(ctx);
 
     if (!identity) {
       return [];
@@ -32,6 +34,6 @@ export const get = query({
     return ctx.db
       .query("projects")
       .withIndex("by_owner", (q) => q.eq("ownerId", identity.subject))
-      .collect();
+      .take(args.limit);
   },
 });
