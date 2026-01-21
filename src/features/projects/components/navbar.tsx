e53b23976d353844fa81d90114bdaa7422fbb1a7
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2026-01-21 11:19:30 +0100

    implement renaming functionality

--- src/features/projects/components/navbar.tsx
+++ src/features/projects/components/navbar.tsx
@@ -13,7 +13,8 @@ import Image from "next/image";
 import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
 import { UserButton } from "@clerk/nextjs";
-import { useProject } from "../hooks/use-projects";
+import { useProject, useRenameProject } from "../hooks/use-projects";
+import { useState } from "react";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -22,6 +23,34 @@ const font = Poppins({
 
 export const Navbar = ({ projectId }: { projectId: Id<"projects"> }) => {
   const project = useProject(projectId);
+  const renameProject = useRenameProject(projectId);
+  const [isRenaming, setIsRenaming] = useState(false);
+  const [name, setName] = useState("");
+
+  const handleStartRename = () => {
+    if (!project) return;
+    setName(project.name);
+    setIsRenaming(true);
+  };
+
+  const handleSubmit = () => {
+    if (!project) return;
+    setIsRenaming(false);
+
+    const trimmedName = name.trim();
+
+    if (!trimmedName || trimmedName === project?.name) return;
+
+    renameProject({ id: projectId, name: trimmedName });
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      handleSubmit();
+    } else if (e.key === "Escape") {
+      setIsRenaming(false);
+    }
+  };
 
   return (
     <nav className="flex justify-between items-center gap-x-2 p-2 bg-sidebar border-b ">
@@ -42,9 +71,25 @@ export const Navbar = ({ projectId }: { projectId: Id<"projects"> }) => {
             </BreadcrumbItem>
             <BreadcrumbSeparator className="ml-0! mr-1!" />
             <BreadcrumbItem>
-              <BreadcrumbPage className="text-sm cursor-pointer hover:text-primary font-medium max-w-40 truncate">
-                {project?.name ?? "Loading..."}
-              </BreadcrumbPage>
+              {isRenaming ? (
+                <input
+                  autoFocus
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  onFocus={(e) => e.currentTarget.select()}
+                  onBlur={handleSubmit}
+                  onKeyDown={handleKeyDown}
+                  className="text-sm bg-transparent text-foreground outline-none focus:ring-1 focus:ring-inset focus:ring-ring font-medium max-w-40 truncate"
+                />
+              ) : (
+                <BreadcrumbPage
+                  onClick={handleStartRename}
+                  className="text-sm cursor-pointer hover:text-primary font-medium max-w-40 truncate"
+                >
+                  {project?.name ?? "Loading..."}
+                </BreadcrumbPage>
+              )}
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
