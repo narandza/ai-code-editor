607103ba40f2bb12fbdfd324f36f8edbcad417ca
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2026-02-09 19:12:10 +0100

    code cleanup

--- src/features/editor/components/editor-view.tsx
+++ src/features/editor/components/editor-view.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import Image from "next/image";
 
 import { useFile, useUpdateFile } from "@/features/projects/hooks/use-files";
@@ -18,6 +18,15 @@ export const EditorView = ({ projectId }: { projectId: Id<"projects"> }) => {
 
   const isActiveFileBinary = activeFile && activeFile.storageId;
   const isActiveFileText = activeFile && !activeFile.storageId;
+
+  // Cleanup pending debounced update on unmount or file change
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, [activeTabId]);
 
   return (
     <div className="h-full flex flex-col">
