35785182efe604dfcbbc7c3dd218bdf8a294c5b1
Dimitrije Jovanovic <dimitrijejovanovic90210@gmail.com>
2026-03-04 14:35:07 +0100

    build github reset api call

---
+++ src/app/api/github/reset/route.ts
@@ -0,0 +1,43 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import z from "zod";
+import { api } from "../../../../../convex/_generated/api";
+import { convex } from "@/lib/convex-client";
+import { Id } from "../../../../../convex/_generated/dataModel";
+
+const requestSchema = z.object({
+  projectId: z.string(),
+});
+
+export async function POST(request: Request) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const body = await request.json();
+  const { projectId } = requestSchema.parse(body);
+
+  const internalKey = process.env.POLARIS_CONVEX_INTERNAL_KEY;
+
+  if (!internalKey) {
+    return NextResponse.json(
+      { error: "Sever configuration error" },
+      { status: 500 },
+    );
+  }
+
+  // Clear export status
+  await convex.mutation(api.system.updateExportStatus, {
+    internalKey,
+    projectId: projectId as Id<"projects">,
+    status: undefined,
+    repoUrl: undefined,
+  });
+
+  return NextResponse.json({
+    success: true,
+    projectId,
+  });
+}
